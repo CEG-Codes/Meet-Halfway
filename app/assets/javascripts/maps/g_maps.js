667d3f7b9a630453eef54826b4db2f8e5b80865d
Jeff Ahking <jeffahking@gmail.com>
2016-09-06 17:05:05 -0400

    adds working autocomplete

--- app/assets/javascripts/maps/g_maps.js
+++ app/assets/javascripts/maps/g_maps.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
   console.log('Hi Sarah!')
-  initAutocomplete();
+  // initAutocomplete();
   buttonlistener();
 });
 
@@ -18,56 +18,6 @@ function buttonlistener() {
     calcRoute();
   })
 }
-
-function initAutocomplete() {
-        // Create the autocomplete object, restricting the search to geographical
-        // location types.
-        autocomplete = new google.maps.places.Autocomplete((document.getElementById('dest1', 'dest2')), {types: ['geocode']});
-
-        // When the user selects an address from the dropdown, populate the address
-        // fields in the form.
-
-  }
-  // function geolocate() {
-  //      if (navigator.geolocation) {
-  //        navigator.geolocation.getCurrentPosition(function(position) {
-  //          var geolocation = {
-  //            lat: position.coords.latitude,
-  //            lng: position.coords.longitude
-  //          };
-  //          var circle = new google.maps.Circle({
-  //            center: geolocation,
-  //            radius: position.coords.accuracy
-  //          });
-  //          autocomplete.setBounds(circle.getBounds());
-  //        });
-  //      }
-  //    }
-
-
-  //function fillInAddress() {
-         // Get the place details from the autocomplete object.
-        // var place = autocomplete.getPlace();
-
-        //  for (var component in componentForm) {
-        //   //  document.getElementById(component).value = '';
-        //   //  document.getElementById(component).disabled = false;
-        //  }
-         //
-        //  // Get each component of the address from the place details
-        //  // and fill the corresponding field on the form.
-        //  for (var i = 0; i < place.address_components.length; i++) {
-        //    var addressType = place.address_components[i].types[0];
-        //    if (componentForm[addressType]) {
-        //      var val = place.address_components[i][componentForm[addressType]];
-        //      document.getElementById(addressType).value = val;
-        //    }
-        //  }
-       //}
-
-
-
-
 
 function initMap()
 {
@@ -88,6 +38,15 @@ function initMap()
 
  map = new google.maps.Map(document.getElementById('map'), map_options);
  directionsDisplay.setMap(map);
+  var input1 = (document.getElementById('dest1'));
+  var input2 = (document.getElementById('dest2'));
+
+  var autocomplete1 = new google.maps.places.Autocomplete(input1);
+      autocomplete1.bindTo('bounds', map);
+  var autocomplete2 = new google.maps.places.Autocomplete(input2);
+      autocomplete2.bindTo('bounds', map);
+
+
  // calcRoute();
 };
 
