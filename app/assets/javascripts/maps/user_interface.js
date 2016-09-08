8c7334ebef5fb680670da9ee299dc8008d0e26f9
sarah aoun <sarah@silk.co>
2016-09-08 16:28:39 -0400

    updates

--- app/assets/javascripts/maps/user_interface.js
+++ app/assets/javascripts/maps/user_interface.js
@@ -33,5 +33,3 @@ function navbarListeners() {
   $('.collapsible').collapsible({
       accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
-}
-
