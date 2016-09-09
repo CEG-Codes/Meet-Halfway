ece4350327dd4ab87a7ea856bf9a20e7f1eca0be
Jeff Ahking <jeffahking@gmail.com>
2016-09-09 16:30:05 -0400

    partial refresh

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,19 @@
 class FavoritesController < ApplicationController
 
 	def create
+    @client = GooglePlaces::Client.new(ENV["googleWebAPI"])
 
     user = current_user.id
-	 	place = params["place"]
-		Favorite.create(user_id: user, place_id: place)
+    place = params["place"]
 
-	end
+    new_favorite = Favorite.create(user_id: user, place_id: place)
+    @favorites = Favorite.all
+
+     respond_to do |format|
+      format.js
+    end
+
+  end
 
 	def delete
 		place = Favorite.find_by_id(params["place"])
