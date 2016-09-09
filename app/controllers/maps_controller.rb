161b291c6f61c248b0a8a7c4df18bb424ad3a15c
Jeff Ahking <jeffahking@gmail.com>
2016-09-09 18:06:50 -0400

    adds opening and closing times

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,14 +1,20 @@
 class MapsController < ApplicationController
-
+include MapsHelper
   def index
+
+    puts what_day_is_it
     @client = GooglePlaces::Client.new(ENV["googleWebAPI"])
+    @results = [];
 
     if current_user
-      @results = [];
       @favorites = Favorite.where(:user_id => current_user.id)
 
+      @favorites.each do |favorite|
+        spot = @client.spot(favorite.place_id)
+        @results.push(spot)
       end
 
+      puts @results
     end
   end
 
