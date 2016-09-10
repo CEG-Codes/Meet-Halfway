cf81639cdb1c8af20afa78e4a6f789fab18526ff
Jeff Ahking <jeffahking@gmail.com>
2016-09-10 11:37:08 -0400

    validation work

--- app/assets/javascripts/maps/validate.js
+++ app/assets/javascripts/maps/validate.js
@@ -1,15 +1,17 @@
 function validate_this()
 {
-  var valid_response = /\s/;
+  //var valid_response = /\s/;
   var input1 = ui.dest1.value;
   var input2 = ui.dest2.value;
-  console.log(ui.dest1.value, ui.dest2.value)
+  console.log(input1, input2)
 
-  if (input1.substr(' ', '') == "" || input2.substr(' ', '') == "")
+  if (input1.replace(" ", "") === "" || input2.replace(" ", "") === "")
   {
+    console.log(input1.replace(" ", ""), input2.replace(" ", ""), 'false')
     return false
   } else
   {
+    console.log('true')
     return true;
   }
 
