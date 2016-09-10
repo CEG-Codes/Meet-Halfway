c732c19d4ea7335c51c2e7bb7a5e7dcfd1e8f884
Robert Crozier <crozierr2@gmail.com>
2016-09-09 20:09:32 -0400

    search window is now replaced cleanly by results

--- app/assets/javascripts/search_results.js
+++ app/assets/javascripts/search_results.js
@@ -1,10 +1,10 @@
-function search_results (){
-<<<<<<< HEAD
-  $('#results').append($result_card)
-=======
-  $('#destSubmitBtn').on('click', function(){
-      $('.search_box').hide();
-  }
-
->>>>>>> 0e2933354f77a1650345c70b440da31716f011a2
-}
+// function search_results (){
+// // <<<<<<< HEAD
+// //   $('#results').append($result_card)
+// // =======
+//   $('#destSubmitBtn').on('click', function(){
+//       $('.search_box').hide();
+//   }
+//
+// >>>>>>> 0e2933354f77a1650345c70b440da31716f011a2
+// }
